bea81f26b97a213b3ef94f3dafbf4ab9641762ab
Abhijeet Saha <abhijeetayon@gmail.com>
2021-01-05 02:28:38 -0500

    inital commit

--- client.c
+++ client.c
@@ -51,6 +51,8 @@ static void sighandler(int signo)
 int main(){
     signal(SIGINT, sighandler);
     handshake();
+    mkfifo("mario", 0666);
+    mkfifo("luigi", 0666);
     int inpipe = open("mario", O_WRONLY);
     if (inpipe == -1) {
         printf("errno: %d\terror: %s\n", errno, strerror(errno));
